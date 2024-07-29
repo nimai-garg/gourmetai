0591d053c0f3b655bbf15de5780ba7fd2e4de86d
Nimai Garg <nimaigarg08@gmail.com>
2024-07-28 19:45:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,18 @@ const fs = require('fs');
  const app = express();
  const port = process.env.PORT || 5001; // Default to 3000 for local development
 
- app.use(cors());
  app.use(bodyParser.json());
-  
+
+ // Use CORS middleware
+app.use(cors({
+  origin: 'http://localhost:3000',  // Your frontend origin
+  methods: 'GET,POST,PUT,DELETE',   // Allowed HTTP methods
+  allowedHeaders: 'Content-Type,Authorization', // Allowed headers
+}));
+
+app.options('*', cors()); // Include this to handle preflight OPTIONS requests
+
+
 const staticPromptPath = path.join(__dirname, 'staticPrompt.txt');
 const nonStaticPromptPath = path.join(__dirname, 'userPrompt.txt');
 
